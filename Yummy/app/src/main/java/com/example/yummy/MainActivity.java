509b9b68fd8bc7dec15eaec40f68786a54f4cec4
Daria <daria.citak1703@gmail.com>
2021-04-22 00:04:09 +0200

    Ekrany poczatkowy i logowania

---
+++ Yummy/app/src/main/java/com/example/yummy/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.yummy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView imageCake;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageCake = findViewById(R.id.imageCake);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.slogan);
+
+        imageCake.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+    }
+}
